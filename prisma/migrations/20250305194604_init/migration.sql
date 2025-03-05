931f7dc0b1a8539d058db45b6881abfba5a4860d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-03-05 16:53:19 -0300

    adding migration

---
+++ prisma/migrations/20250305194604_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "ApiData" ADD COLUMN     "updatedAt" TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "ApiKeys" ADD COLUMN     "updatedAt" TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "Data" ADD COLUMN     "updatedAt" TIMESTAMP;
