fbc45b7d24c0ae21027cee2bfa3d93a4a7aa58cc
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-03-12 16:09:57 -0300

    feat: adding some changes

--- src/controllers/usercolab.controller.ts
+++ src/controllers/usercolab.controller.ts
@@ -9,6 +9,7 @@ import { validateUser } from '../schemas/usercolab.validation';
 ///// TODO: Check if the user is admin to access this class methods
 /**
  * UserColabController class for UserColab Table CRUD methods
+ * ‼️Only Super-Admin
  */
 export class UserColabController{
     /**
@@ -23,12 +24,17 @@ export class UserColabController{
         const {username, password, isSuperAdmin}:Pick<UserColab, "username" | "password" | "isSuperAdmin"> = req.body
        try{
         const validate = validateUser(req.body)
-                    if(!validate.success) res.status(StatusCodes.BAD_REQUEST).json({ message: validate.error.message })
+                    if(!validate.success){
+                        res.status(StatusCodes.BAD_REQUEST).json({ message: validate.error.message })
+                        return
+                    } 
         
                     const uniqueUserColab = await prisma.userColab.findUnique({where: {username}})
         
-                    if(uniqueUserColab) res.status(StatusCodes.BAD_REQUEST).json({message: 'Username already exists'})
-        
+                    if(uniqueUserColab) {
+                        res.status(StatusCodes.BAD_REQUEST).json({message: 'Username already exists'})
+                        return
+                    }
                     const hashedPassword = bcrypt.hashSync(password, 10)
         
                     const newUserColab = await prisma.userColab.create({
@@ -49,7 +55,16 @@ export class UserColabController{
        }
     }
 
-    async showUserColab(req:Request, res: Response, next: NextFunction){
+    /**
+     * 
+     * Listing UserColab Records & count -- Only Super-Admin ---
+     * @param {Request} req --> The HTTP request object: appName, appId and dataId
+     * @param {Response} res --> Response object to the client
+     * @param {NextFunction} next --> The next middleware function for error handling.
+     * @returns {Promise<void>} --> Sends a response indicating success or validation failure.
+     */
+
+    async showUserColab(req:Request, res: Response, next: NextFunction):Promise<void>{
         try{
  
         }catch(error){
@@ -59,8 +74,43 @@ export class UserColabController{
          })
         }
      }
+   /**
+     * 
+     * Select a single UserColab record -- Only Super-Admin ---
+     * @param {Request} req --> The HTTP request object: appName, appId and dataId
+     * @param {Response} res --> Response object to the client
+     * @param {NextFunction} next --> The next middleware function for error handling.
+     * @returns {Promise<void>} --> Sends a response indicating success or validation failure.
+     */ 
+     async selectUserColab(req:Request, res: Response, next: NextFunction):Promise<void>{
+        const {id} = req.body
+       try{
+          const userColab = await prisma.userColab.findFirst({where:{id: id}})
 
-     async updateUserColab(req:Request, res: Response, next: NextFunction){
+          // Handle --> Not results found
+          if(!userColab){
+            res.status(StatusCodes.OK).json({message: 'User not found'})
+            return
+          }
+          res.status(StatusCodes.OK).json({ user: userColab })
+          return
+       }catch(error){
+        return next({
+            status: StatusCodes.BAD_GATEWAY,
+            message: `Something went wrong --> Error: ${error}`
+        })
+       }
+     }
+   
+    /**
+     * 
+     * Listing UserColab Records & count -- Only Super-Admin ---
+     * @param {Request} req --> The HTTP request object: appName, appId and dataId
+     * @param {Response} res --> Response object to the client
+     * @param {NextFunction} next --> The next middleware function for error handling.
+     * @returns {Promise<void>} --> Sends a response indicating success or validation failure.
+     */ 
+     async updateUserColab(req:Request, res: Response, next: NextFunction):Promise<void>{
         try{
  
         }catch(error){
