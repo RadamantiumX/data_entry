5c4a4186ff8e436d0feb6cb1da28b24999cf0e50
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-03-16 19:12:35 -0300

    feat: testing

--- src/errors/prisma.errors.ts
+++ src/errors/prisma.errors.ts
@@ -6,7 +6,7 @@ import { PRISMA_ERROR_CODES } from "./prisma_errors_codes/prisma.codes";
 // TODO: Look at this to guidance: https://www.toptal.com/nodejs/node-js-error-handling
 // Read Prisma docs: https://www.prisma.io/docs/orm/prisma-client/debugging-and-troubleshooting/handling-exceptions-and-errors
 
-export const prismaError = (error:PrismaErrorType) => {
+export const prismaError = (error:any) => {
    for(const [key, value] of Object.entries(PRISMA_ERROR_CODES)){
      if(key === error.code){
         return { error_data: error, error_message: value }
