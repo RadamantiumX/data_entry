7355cdf9c220b96d186cf16d35fb7426578fa03d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-03-22 18:50:05 -0300

    feat: adding querys files with empty fn

---
+++ src/services/prisma_querys/datum.query.ts
@@ -0,0 +1,26 @@
+import { prisma } from "../../db/prisma.db";
+import { ApiKey } from "../../types/types";
+import bcrypt from 'bcryptjs'
+import { getTimestampParsed } from "../../helper/time.helper";
+
+
+export const createRecord = async () => {
+
+    
+}
+
+export const readCountRecords = async () => {
+  
+}
+
+export const readRecord = async ()  => {
+  
+}
+
+export const updateRecord = async () => {
+   
+}
+
+export const destroyRecord = async () => {
+ 
+}
