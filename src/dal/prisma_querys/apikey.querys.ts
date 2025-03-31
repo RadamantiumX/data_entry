9c5881889d1b22490950e45655dec91bbc6cdfed
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-03-31 16:30:47 -0300

    feat: converto to class

--- src/dal/prisma_querys/apikey.querys.ts
+++ src/dal/prisma_querys/apikey.querys.ts
@@ -3,22 +3,23 @@ import { getTimestampParsed } from "../../helper/time.helper";
 import { ApiKey, ApiKeyClientResponse } from "../../types/types";
 
 
-/**
+export class ApiKeyQuerys{
+  /**
  * Create a new record ApiKey Prisma model record
- * @param {Omit<ApiKey, "id" | "createdAt" | "updatedAt" >} bodyRequest 
+ * @param {Omit<ApiKey, "id" | "createdAt" | "updatedAt" >} payload 
  * @returns {Promise<void>}
  */
-export const createRecord = async (bodyRequest:Omit<ApiKey, "id" | "createdAt" | "updatedAt" >): Promise<void> => {
+  static async createRecord (payload:Omit<ApiKey, "id" | "createdAt" | "updatedAt" >): Promise<void>{
   // Save a new record
    await prisma.apiKeys.create({
     data:{
-      apiKey: bodyRequest.apiKey,
-      apiKeySecret: bodyRequest.apiKeySecret,
-      bearerToken: bodyRequest.bearerToken,
-      accessToken: bodyRequest.accessToken,
-      accessTokenSecret: bodyRequest.accessTokenSecret,
-      apiDataId: bodyRequest.apiDataId,
-      dataId: bodyRequest.dataId
+      apiKey: payload.apiKey,
+      apiKeySecret: payload.apiKeySecret,
+      bearerToken: payload.bearerToken,
+      accessToken: payload.accessToken,
+      accessTokenSecret: payload.accessTokenSecret,
+      apiDataId: payload.apiDataId,
+      dataId: payload.dataId
     }
   
  })
@@ -29,7 +30,7 @@ export const createRecord = async (bodyRequest:Omit<ApiKey, "id" | "createdAt" |
  * Return all records from the ApiKey Prisma model
  * @returns {Promise<ApiKeyClientResponse>} Collection of ApiKey (only selected fields) and the total count of tha
  */
-export const readCountRecords = async ():Promise<ApiKeyClientResponse> => {
+  static async readCountRecords ():Promise<ApiKeyClientResponse>{
     const [apiKeys, totalApiKeys] = await prisma.$transaction([
         prisma.apiKeys.findMany({omit:{id: true, createdAt: true, updatedAt: true}, orderBy: {createdAt: 'desc'}}),
         prisma.apiKeys.count()
@@ -43,27 +44,27 @@ export const readCountRecords = async ():Promise<ApiKeyClientResponse> => {
  * @param {Pick<ApiKey, "id">} paramRequest Take the id from the ApiKey Prisma model
  * @returns {Promise<Omit<ApiKey, "id" | "createdAt" | "updatedAt"> | null>} A single record and, only selected fields
  */
-export const readRecord = async (paramRequest:Pick<ApiKey, "id">):Promise<Omit<ApiKey, "id" | "createdAt" | "updatedAt"> | null>  => {
+static async readRecord (paramRequest:Pick<ApiKey, "id">):Promise<Omit<ApiKey, "id" | "createdAt" | "updatedAt"> | null> {
   const apiKey = await prisma.apiKeys.findUnique({where:{id: paramRequest.id}, omit:{id:true, updatedAt: true, createdAt: true}})
   return apiKey
 }
 
 /**
  * Update the selected record from the provided id
- * @param {Omit<ApiKey, "createdAt" | "updatedAt">} bodyRequest 
+ * @param {Omit<ApiKey, "createdAt" | "updatedAt">} payload 
  * @returns {Promise<void>}
  */
-export const updateRecord = async (bodyRequest:Omit<ApiKey, "createdAt" | "updatedAt">):Promise<void> => {
+ static async updateRecord (payload:Omit<ApiKey, "createdAt" | "updatedAt">):Promise<void> {
  await prisma.apiKeys.update({
     where:{
-      id: bodyRequest.id
+      id: payload.id
     },
     data:{
-      apiKey: bodyRequest.apiKey,
-      apiKeySecret: bodyRequest.apiKeySecret,
-      bearerToken: bodyRequest.bearerToken,
-      accessToken: bodyRequest.accessToken,
-      accessTokenSecret: bodyRequest.accessTokenSecret,
+      apiKey: payload.apiKey,
+      apiKeySecret: payload.apiKeySecret,
+      bearerToken: payload.bearerToken,
+      accessToken: payload.accessToken,
+      accessTokenSecret: payload.accessTokenSecret,
       updatedAt: getTimestampParsed()
     }
   })
@@ -73,10 +74,12 @@ export const updateRecord = async (bodyRequest:Omit<ApiKey, "createdAt" | "updat
 
 /**
  * Delete a single record from the ApiKey Prisma model
- * @param {Pick<ApiKey, "id">} bodyRequest Take the id from the model
+ * @param {Pick<ApiKey, "id">} payload Take the id from the model
  * @returns {Promise<void>}
  */
-export const destroyRecord = async (bodyRequest:Pick<ApiKey, "id">):Promise<void> => {
-   await prisma.apiKeys.delete({where:{id: bodyRequest.id}})
+static async destroyRecord (payload:Pick<ApiKey, "id">):Promise<void> {
+   await prisma.apiKeys.delete({where:{id: payload.id}})
    return
 }
+}
+
