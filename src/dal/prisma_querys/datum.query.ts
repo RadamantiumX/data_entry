991809379c9086469f4c025f587f53f64112e817
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-03-31 18:44:33 -0300

    feat: turn to class

--- src/dal/prisma_querys/datum.query.ts
+++ src/dal/prisma_querys/datum.query.ts
@@ -2,12 +2,14 @@ import { prisma } from "../../db/prisma.db";
 import { getTimestampParsed } from "../../helper/time.helper";
 import { Datum, DatumClientResponse, AllRelatedData } from "../../types/types";
 
-/**
+
+export class DatumQuerys {
+  /**
  * Create a new record from the Datum Prisma model
  * @param {Omit<Datum, "id" | "createdAt" | "updatedAt">} bodyRequest 
  * @returns {Promise<void>}
  */
-export const createRecord = async (bodyRequest:Omit<Datum, "id" | "createdAt" | "updatedAt">):Promise<void> => {
+ static async createRecord (bodyRequest:Omit<Datum, "id" | "createdAt" | "updatedAt">):Promise<void> {
  // Save a new record
  await prisma.data.create({
     data:{
@@ -27,7 +29,7 @@ export const createRecord = async (bodyRequest:Omit<Datum, "id" | "createdAt" | 
  * Return all records from Datum Prisma model, only selected fields
  * @returns {Promise<DatumClientResponse>}
  */
-export const readCountRecords = async ():Promise<DatumClientResponse> => {
+   static async readCountRecords ():Promise<DatumClientResponse>  {
     const [data, totalData] = await prisma.$transaction([
         prisma.data.findMany({omit:{ createdAt: true, updatedAt: true}, orderBy: {createdAt: 'desc'}}), // Descendent order by the timestamp "createdAt"
         prisma.data.count()
@@ -42,7 +44,7 @@ export const readCountRecords = async ():Promise<DatumClientResponse> => {
  * @param {Pick<Datum, "id">} paramRequest 
  * @returns {Promise<Omit<Datum, "createdAt"| "updatedAt"> | null > }
  */
-export const readRecord = async (paramRequest:Pick<Datum, "id">):Promise<Omit<Datum, "createdAt"| "updatedAt"> | null >  => {
+ static async readRecord  (paramRequest:Pick<Datum, "id">):Promise<Omit<Datum, "createdAt"| "updatedAt"> | null > {
     const singleData = await prisma.data.findUnique({where:{id: paramRequest.id}, omit:{ updatedAt: true, createdAt: true}})
     return singleData
 }
@@ -53,7 +55,7 @@ export const readRecord = async (paramRequest:Pick<Datum, "id">):Promise<Omit<Da
  * @param {Pick<Datum, "emailSource">} paramRequest emailSource unique on table
  * @returns {Promise<Omit<Datum, "createdAt"| "updatedAt"> | null >}
  */
-export const readUniqueEmail = async (paramRequest:Pick<Datum, "emailSource">):Promise<Omit<Datum, "createdAt"| "updatedAt"> | null > => {
+  async readUniqueEmail (paramRequest:Pick<Datum, "emailSource">):Promise<Omit<Datum, "createdAt"| "updatedAt"> | null >  {
   const singleData = await prisma.data.findUnique({where: {emailSource: paramRequest.emailSource}, omit: {updatedAt: true, createdAt: true}})
   return singleData
   
@@ -63,7 +65,7 @@ export const readUniqueEmail = async (paramRequest:Pick<Datum, "emailSource">):P
  * Return all related data from all models --Apidata | ApiKeys | Datum-- and relations
  * @returns {:Promise<AllRelatedData []>}
  */
-export const readAllRelated = async ():Promise<AllRelatedData []> =>{
+  static async readAllRelated ():Promise<AllRelatedData []> {
      // Select the record and nested data (ApiData & ApiKey)
      const allRecords = await prisma.data.findMany({ select:{id:true, emailSource: true, emailSourcePsw: true, xUser: true, xPsw:true ,
       apiData:{      
@@ -83,7 +85,7 @@ export const readAllRelated = async ():Promise<AllRelatedData []> =>{
  * @param {Omit<Datum, "updatedAt" | "createdAt">} bodyRequest 
  * @returns {Promise<void>}
  */
-export const updateRecord = async (bodyRequest:Omit<Datum, "updatedAt" | "createdAt">):Promise<void> => {
+ static async updateRecord (bodyRequest:Omit<Datum, "updatedAt" | "createdAt">):Promise<void> {
     await prisma.data.update({
         where:{
           id: bodyRequest.id
@@ -106,7 +108,9 @@ export const updateRecord = async (bodyRequest:Omit<Datum, "updatedAt" | "create
  * @param {Pick<Datum, "id">} bodyRequest 
  * @returns {Promise<void>}
  */
-export const destroyRecord = async (bodyRequest:Pick<Datum, "id">):Promise<void> => {
+ static async destroyRecord (bodyRequest:Pick<Datum, "id">):Promise<void> {
     await prisma.data.delete({where:{id: bodyRequest.id}})
     return
 }
+}
+
