cb92f246b43e5d98fd7a1f77af0a5f41d83ea03b
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-03-31 19:30:48 -0300

    feat: adding services methods on controller

--- src/controllers/datum.controller.ts
+++ src/controllers/datum.controller.ts
@@ -1,9 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { prisma } from '../db/prisma.db';
-import { validateDatum } from '../schemas/datum.validation';
-import { createRecord, readCountRecords, readRecord, updateRecord, destroyRecord, readAllRelated, readUniqueEmail } from '../dal/prisma_querys/datum.query';
-
+import { DatumService } from '../services/datum.service';
 
 /**
  * Controller Class For APIDATA Operations
@@ -27,9 +24,7 @@ export class DatumController{
      */
     async saveDatum(req:Request, res: Response, next: NextFunction):Promise<void>{
         try{
-           // Body validation & handle conditional
-           await validateDatum(req.body)
-           await createRecord(req.body)
+           await DatumService.createDatum(req.body)
            res.status(StatusCodes.OK).json({ message: "Success on saving data" })
            return
         }catch(error){
@@ -48,7 +43,7 @@ export class DatumController{
    async showDatum(req:Request, res: Response, next: NextFunction):Promise<void> {
      try{
         // Select query for all records on related table model
-        const datum = await readCountRecords()
+        const datum = await DatumService.getAllDatum()
         res.status(StatusCodes.OK).json( datum.totalData > 0 ? { data: datum.data, count: datum.totalData } : {message: 'No data founded'})
         return
      }catch(error){
@@ -65,10 +60,7 @@ export class DatumController{
      */ 
    async updateDatum(req: Request, res: Response, next: NextFunction):Promise<void>{
     try{
-        // Body validation & handle conditional
-        await validateDatum(req.body)
-        
-        await updateRecord(req.body)
+        await DatumService.updateDatum(req.body)
         res.status(StatusCodes.OK).json({ message: 'Success on update data' })
         return
     }catch(error){
@@ -87,7 +79,7 @@ export class DatumController{
     
      try{
         // Find the current record with the "emailSource" provided
-        const uniqueRecordEmail = await readUniqueEmail({emailSource:req.params.email}) 
+        const uniqueRecordEmail = await DatumService.getUniqueEmailSourceDatum(req.body)
         res.status(StatusCodes.OK).json(uniqueRecordEmail ? {uniqueRecordEmail}: {message: 'No resource founded...'})
      }catch(error){
         return next(error)
@@ -106,7 +98,7 @@ export class DatumController{
    async selectAllRelated(req:Request, res: Response, next: NextFunction):Promise<void>{
     try{
         // Select the record and nested data (ApiData & ApiKey)
-        const allRelatedRecords = await readAllRelated()
+        const allRelatedRecords = await DatumService.getAllRelated()
         res.status(StatusCodes.OK).json(allRelatedRecords.length > 0 ? { allRelatedRecords } : { message: 'No resource founded...' })
         return
     }catch(error){
@@ -125,7 +117,7 @@ export class DatumController{
      */ 
    async destroyDatum(req:Request, res: Response, next: NextFunction):Promise<void>{
     try{
-        await destroyRecord(req.body)
+        await DatumService.destroyDatum(req.body)
         res.status(StatusCodes.OK).json({message: 'Record deleted...'})
         return
     }catch(error){
