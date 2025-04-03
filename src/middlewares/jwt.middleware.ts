0c26177b36c1bec10b17372cf8cbed5d016e6a9d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-02 23:29:58 -0300

    feat: some change

---
+++ src/middlewares/jwt.middleware.ts
@@ -0,0 +1,7 @@
+import { SECRET_KEY } from "../utils/jwt.methods"
+import jwt from 'jsonwebtoken'
+import { JsonWebTokenError } from "jsonwebtoken"
+
+export const verifyToken = (authToken:string) => {
+    
+}
--- src/utils/jwt.methods.ts
+++ src/utils/jwt.methods.ts
@@ -5,13 +5,13 @@ import type { IPayload } from '../types/types'
 // TODO: Use the callback for generate a two separates TOKENS, ACCESS_TOKEN & REFRESH_TOKEN
 // Adapt the provided projecto to replicate the funtioanality
 
-const SECRET:Readonly<string> = process.env.JWT_64 || 'secret'
+export const SECRET_KEY:Readonly<string> = process.env.JWT_64 || 'secret'
 /**
  * JWT methods
  */
 export default {
     sign: (payload: IPayload) => 
-        jwt.sign(payload, SECRET, {expiresIn: '7d', algorithm: 'HS256'}),
+        jwt.sign(payload, SECRET_KEY, {expiresIn: '7d', algorithm: 'HS256'}),
 
-    verify: (token: string) => jwt.verify(token, SECRET)
+    verify: (token: string) => jwt.verify(token, SECRET_KEY)
 }
