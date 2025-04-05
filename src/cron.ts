103ec7ae452e775681dc44345d4b2c6dfd17141f
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-05 15:59:12 -0300

    feat: adding comment

--- src/cron.ts
+++ src/cron.ts
@@ -1,5 +1,5 @@
 import cron from 'node-cron'
-
+// TODO: for the next project---> Apply CRON JOBS to remove records on DB when having pass 24 hs
 // Space into *
 cron.schedule('* * * * *', async ()=>{
     console.log('Runing every minute')
