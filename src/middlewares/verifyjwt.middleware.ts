4d2caa178bae20aa5f32d3ebb4050e85e8470faf
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-10 10:00:05 -0300

    feat: adding some changes

--- src/middlewares/verifyjwt.middleware.ts
+++ src/middlewares/verifyjwt.middleware.ts
@@ -2,12 +2,18 @@ import { SECRET_KEY } from "../utils/jwt.methods"
 import jwt from 'jsonwebtoken'
 import { JsonWebTokenError } from "jsonwebtoken"
 import { Request, Response, NextFunction } from "express"
+import { StatusCodes } from "http-status-codes"
 
 export const verifyJWT = async (req:Request, res:Response, next:NextFunction):Promise<void> => {
     const authHeader = req.headers.authorization
+    const cookies = req.cookies
     try{
+        if(!authHeader || !cookies.jwt){
+            res.status(StatusCodes.FORBIDDEN).json({code: 403, message:"Missing credentials"})
+            return
+        }
 
-        res.cookie
+        
     }catch(error){
         return next(error)
     }
