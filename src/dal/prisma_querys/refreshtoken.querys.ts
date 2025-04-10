b0c70e695224ec1a300b18fb5ba882cd1c7bb3a4
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-10 10:00:18 -0300

    feat: adding some changes

--- src/dal/prisma_querys/refreshtoken.querys.ts
+++ src/dal/prisma_querys/refreshtoken.querys.ts
@@ -1,5 +1,9 @@
+import { prisma } from "../../db/prisma.db";
+import { AuthRefreshToken } from "../../types/types";
+
 export class RefreshTokenQuerys{
-    async checkingRecord(refToken:string){
-        
+    async checkingRecord(userId:string,refToken:string):Promise<Pick<AuthRefreshToken, 'id'> | null>{
+        const checkToken = await prisma.authRefreshToken.findUnique({where:{userColabId: userId , refreshToken: refToken}, select:{id:true}})
+        return checkToken
     }
 }
