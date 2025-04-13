b0a82d32f2b1a1e7ed765c640eb2b5aa79efd8d8
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-13 19:57:19 -0300

    feat: fixing services

--- src/middlewares/verifyjwt.middleware.ts
+++ src/middlewares/verifyjwt.middleware.ts
@@ -1,14 +1,14 @@
-import { RerfreshTokenService } from "../services/refreshtoken.service"
 import { Request, Response, NextFunction } from "express"
 import { StatusCodes } from "http-status-codes"
 import { AccessTokenService } from "../services/accesstoken.service"
+import { UserColab } from "../types/types"
 
 
 // TODO: add TOKEN verifing before decoding token
 export const verifyJWT = async (req:Request, res:Response, next:NextFunction):Promise<void> => {
     const authHeader = req.headers.authorization
     try{
-        const validateToken = await AccessTokenService.checkOwnerCredentials(authHeader)
+        const validateToken:Pick<UserColab, "isSuperAdmin"|"id"> | null = await AccessTokenService.checkOwnerCredentials(authHeader)
         if(!validateToken){
             res.status(StatusCodes.UNAUTHORIZED).json({message: 'invalid credentials provided'})
         }
--- src/services/accesstoken.service.ts
+++ src/services/accesstoken.service.ts
@@ -1,12 +1,13 @@
 import { validateAccessToken } from "../schemas/accesstoken.validation"
 import { JWTverifyAndDecode } from "../helper/jwt.helper"
-import { prisma } from "../db/prisma.db"
+import { AuthQuerys } from "../dal/prisma_querys/auth.querys"
+import { UserColab } from "../types/types"
 
 export class AccessTokenService{
-    static async checkOwnerCredentials(authToken:string | any){
+    static async checkOwnerCredentials(authToken:string | any):Promise<Pick<UserColab, "isSuperAdmin" | "id"> | null>{
           await validateAccessToken(authToken)
-          const decodedInfo = JWTverifyAndDecode(authToken?.split(' ')[1])
-          const userColab = await prisma.userColab.findUnique({where:{ username: decodedInfo.username }, select: {id: true}})
+          const decodedInfo:any = JWTverifyAndDecode(authToken?.split(' ')[1])
+          const userColab = await AuthQuerys.checkingRecord(decodedInfo.id)
           return userColab
       }
 }
