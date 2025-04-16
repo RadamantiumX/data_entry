1b4753f65a273254119163d1e013028a4ce09f01
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-16 09:31:44 -0300

    feat: some fix

--- src/constants/index.constants.ts
+++ src/constants/index.constants.ts
@@ -4,4 +4,4 @@ export const COOKIE_EXP:Date = new Date(Date.now() + 24 * 60 * 60 * 1000)
 export const A_TOKEN_TIME:string = process.env.ACCESS_TOKEN_EXPIRATION_TIME || '10s'
 export const R_TOKEN_TIME:string = process.env.REFRESH_TOKEN_EXPIRATION_TIME || '1d'
 
-export const UNIX_TIME_EXPIRATION = new Date().getTime() / 1000 // Equals with the value of REFRESH TOKEN EXPIRATION
+export const UNIX_TIME_EXPIRATION = new Date().getTime() / 1000 // Today NOW TIMESTAMPS --> Equals with the value of REFRESH TOKEN EXPIRATION 
