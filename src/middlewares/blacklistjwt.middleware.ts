72bfb620c79448fb8655251da57c4ac99fb532ed
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-17 11:34:18 -0300

    feat: set blacklist middelware

--- src/middlewares/blacklistjwt.middleware.ts
+++ src/middlewares/blacklistjwt.middleware.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
+import { RerfreshTokenService } from '../services/refreshtoken.service';
+import { StatusCodes } from 'http-status-codes';
 
 
-export const blackListJWT = (req:Request, res:Response, next:NextFunction) => {
+export const blackListJWT = async (req:Request, res:Response, next:NextFunction) => {
     const cookies = req.cookies
    try{
     const refreshToken = cookies.jwt
-
+    const { isValid } = await RerfreshTokenService.blackListVerify(refreshToken)
+    if(!isValid){
+        res.status(StatusCodes.UNAUTHORIZED).json({code:403, message:'The credentials provided is invalid'})
+        return
+    }
+    next()
    }catch(error){
     next(error)
    }
