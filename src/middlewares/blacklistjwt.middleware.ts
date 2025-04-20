a32cb15898e3edbc3635ab12aa0469ec7a289886
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-20 16:59:42 -0300

    feat: finish the blacklist middleware

--- src/middlewares/blacklistjwt.middleware.ts
+++ src/middlewares/blacklistjwt.middleware.ts
@@ -2,8 +2,17 @@ import { NextFunction, Request, Response } from 'express';
 import { RerfreshTokenService } from '../services/refreshtoken.service';
 import { StatusCodes } from 'http-status-codes';
 
-
-export const blackListJWT = async (req:Request, res:Response, next:NextFunction) => {
+/**
+ * Hanlde the BLACKLIST of the JWT
+ * Using the "issuedAt" property (incorporated on decoded JWT), can be do a constranit
+ * if this LIFETIME is minor, the token is deleted.
+ * 
+ * @param {Request} req ---> Request cookies from the client
+ * @param {Response} res ---> Send a JSON with the header
+ * @param {NextFunction} next --> Next middleware
+ * @returns {Promise<void>}
+ */
+export const blackListJWT = async (req:Request, res:Response, next:NextFunction):Promise<void> => {
     const cookies = req.cookies
    try{
     const refreshToken = cookies.jwt
