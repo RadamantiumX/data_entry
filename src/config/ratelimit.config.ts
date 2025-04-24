a3e84f6245800ddbd7531156688561dc516b9073
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-04-23 23:31:13 -0300

    feat: some comment

--- src/config/ratelimit.config.ts
+++ src/config/ratelimit.config.ts
@@ -1,5 +1,8 @@
 import rateLimit from "express-rate-limit";
 
+
+// TODO: First, look up the documentation: https://express-rate-limit.mintlify.app/overview
+
 export const loginAttempsLimiter = rateLimit({
-    
+
 })
