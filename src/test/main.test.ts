e3344bb83510c2cbbcd81757a5e5e94dc104da0e
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-05-01 21:52:14 -0300

    feat: some test

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -12,7 +12,7 @@ describe("test", ()=> {
 })
 
 describe("GET /", ()=>{
-    it('should return 403 Forbidden', async ()=> {
+    it('should return 200 OK', async ()=> {
         const response = await request(app).get('/')
         expect(response.status).toBe(200)
          expect(response.body.message).toBe('server on')
